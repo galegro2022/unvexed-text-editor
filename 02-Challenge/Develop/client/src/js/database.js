ba2b5c9e32d0f040c19a2d1369a9fc8b27d9e0ca
Alegro Adimora Godley Godley <alegroadimoragodleygodley@Alegros-MacBook-Pro.local>
2023-03-06 21:33:03 -0500

    almost done

--- 02-Challenge/Develop/client/src/js/database.js
+++ 02-Challenge/Develop/client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Export a function we will use to Put something in the database.
+export const putDb = async (content) => {
+// Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+  // Create a transaction to the database.
+  const tx = jateDb.transaction("jate", "readwrite");
+  // Create a store to the database.
+  const store = tx.objectStore("jate");
+  // Add the content to the database.
+  const request = store.put({id: 1, value: content});
+  // Wait for the request to complete.
+  const result = await request;
+
+  console.log("updated text saved", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getAllDb = async () => {
+const JateDb = await openDB('jate', 1);
+const tx = JateDb.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request;
+console.log("result.value", result.value);
+return result.value;
+};
 
 initdb();
